b18ac67e9cbbb5b1bd4c5ba11f88b1b1a4145c43
Nerldy_ <arnodhis@gmail.com>
2018-04-11 18:36:04 +0300

    added logic to delete and update book plus end points

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ def api_get_all_books():
 
 @app.route('/api/v1/books/<string:book_id>')
 def api_get_single_book(book_id):
+	if book_id.isalnum() is False:
+		return jsonify({"error": "make sure you're using web safe characters"}), 400
 	single_book = [book for book in books_collection if book['id'] == book_id]
 
 	if len(single_book) < 1:
@@ -96,10 +98,10 @@ def api_create_book():
 	if isinstance(req_data['author'], list) is False:
 		return jsonify({'error': 'author must be of list type'}), 400
 
-	# Validate ISBN
+	# validate ISBN
 	isbn_data = req_data['isbn'].strip()
 	if len(isbn_data) < 1:
-		return jsonify({'error': "ISBN field cannot be empty"})
+		return jsonify({'error': "ISBN field cannot be empty"}), 400
 
 	isbn_data_split = isbn_data.split("-")
 	isbn_join = "".join(isbn_data_split)
@@ -109,9 +111,9 @@ def api_create_book():
 
 	if (len(isbn_join) != 10) and (len(isbn_join) != 13):
 		return jsonify(
-			{"error": "make sure the digits part of the ISBN(without the -), are either 10 digits or 13 digits"})
+			{"error": "make sure the digits part of the ISBN(without the -), are either 10 digits or 13 digits"}), 400
 
-	# format author
+	# validate author
 	new_author = []
 	for author in req_data['author']:
 		if len(author.strip()) < 1:
@@ -120,6 +122,10 @@ def api_create_book():
 			new_author.append(format_book_input_values(author))
 
 	req_data['author'] = new_author
+
+	# validate synopsis
+	if len(req_data['synopsis']) < 50:
+		return jsonify({"error": "synopsis must be at least 50 characters long"}), 400
 
 	single_book = {  # Book blueprint
 		'id': uuid.uuid4().hex,
@@ -132,6 +138,73 @@ def api_create_book():
 
 	books_collection.append(single_book)  # add to book collection
 	return jsonify({'Book successfully created': single_book})
+
+
+@app.route('/api/v1/books/<string:book_id>', methods=['PUT'])
+def api_update_book(book_id):
+	"""
+	updates book
+	:param book_id:
+	:return:
+	"""
+	if book_id.isalnum() is False:
+		return jsonify({"error": "make sure you're using web safe characters"}), 400
+
+	update_book = [book for book in books_collection if book['id'] == book_id]
+
+	req_data = request.get_json()
+	if not request.json:
+		abort(400)
+
+	if 'title' in request.json:
+		title = format_book_input_values(req_data['title'])
+
+		if len(req_data['title']) < 1:
+			abort(400)
+		else:
+			request.json['title'] = title
+
+	if 'author' in request.json:
+		if isinstance(req_data['author'], list) is False:
+			abort(400)
+
+		# validate author
+		new_author = []
+		for author in request.json['author']:
+			if len(author.strip()) < 1:
+				return jsonify({'error': 'author list cannot contain an empty field'}), 400
+			else:
+				new_author.append(format_book_input_values(author))
+
+		request.json['author'] = new_author
+
+	if 'synopsis' in request.json:
+		# validate synopsis
+		if len(request.json['synopsis']) < 50:
+			abort(400)
+
+	req_data_key_list = req_data.keys()
+
+	if len(allowed_keys) < len(req_data_key_list):  # filter unrecognized keys
+		for key in req_data:
+			if key not in allowed_keys:
+				return jsonify({"error": f"{key} is an unrecognized key"}), 400
+
+	update_book[0]['title'] = request.json.get('title', update_book[0]['title'])
+	update_book[0]['author'] = request.json.get('author', update_book[0]['author'])
+	update_book[0]['synopsis'] = request.json.get('synopsis', update_book[0]['synopsis'])
+	return jsonify({"Book updated": update_book[0]})
+
+
+@app.route('/api/v1/books/<string:book_id>', methods=['DELETE'])
+def api_delete_book(book_id):
+	delete_book = [book for book in books_collection if book['id'] == book_id]
+
+	if len(delete_book) < 1:
+		abort(404)
+
+	books_collection.remove(delete_book[0])
+	return jsonify({"result": True})
 
 
 if __name__ == '__main__':
